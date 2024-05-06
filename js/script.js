6418e6fc42711500c8fa3199039bfba15a82cec2
adrianjanka <adi.janka@bluewin.ch>
2024-05-06 14:03:48 +0200

    some changes

--- js/script.js
+++ js/script.js
@@ -26,18 +26,21 @@ async function fetchRandomDrink() {
 // Funktion zum Anzeigen des zufälligen Getränks auf der Seite
 function displayDrink(drink) {
     // get infos from drink object
-  const drinkName = drink.strDrink;
-  const instructions = drink.strInstructionsDE;
-  const ingredients = [];
+  let drinkImage = drink.strDrinkThumb;
+  let drinkName = drink.strDrink;
+  let instructions = drink.strInstructionsDE;
+  let ingredients = [];
   for (let i = 1; i <= 15; i++) {
-    const ingredient = drink['strIngredient' + i];
-    const measure = drink['strMeasure' + i];
+    let ingredient = drink['strIngredient' + i];
+    let measure = drink['strMeasure' + i];
+    measure = converter(measure);
     if (ingredient && measure) {
       ingredients.push(`${measure.trim()} ${ingredient.trim()}`);
     }
   }
 
   // display drink on page
+  document.getElementById('drinkImage').src = drinkImage;
   document.getElementById('drinkName').textContent = `Gsöff vom Tag: ${drinkName}`;
   document.getElementById('drinkInstructions').textContent = `Zubereitung: ${instructions}`;
   document.getElementById('drinkIngredients').innerHTML = ingredients.map(ingredient => `<li>${ingredient}</li>`).join('');
@@ -67,18 +70,18 @@ async function fetchPersonalDrink() {
       let drink = drinks[randomIndex];
       displayPersonalDrink(drink);
     } else {
-      document.getElementById('personalDrink').textContent = `Tschuldigung, hüt gits nüt für di: ${name}.`;
+      document.getElementById('personalDrink').textContent = `Tschuldigung, ${name} hüt gits nüt für di.`;
     }
 
   }
 
   // Funktion zum Anzeigen des personalisierten Getränks auf der Seite
   function displayPersonalDrink(drink) {
-    const drinkName = drink.strDrink;
-    const instructions = drink.strInstructionsDE;
-    const ingredients = [];
+    let drinkName = drink.strDrink;
+    let instructions = drink.strInstructionsDE;
+    let ingredients = [];
     for (let i = 1; i <= 15; i++) {
-      const ingredient = drink['strIngredient' + i];
+      let ingredient = drink['strIngredient' + i];
       let measure = drink['strMeasure' + i];
       measure = converter(measure);
       if (ingredient && measure) {
@@ -86,11 +89,24 @@ async function fetchPersonalDrink() {
       }
     }
 
-    document.getElementById('personalDrink').innerHTML = `
-      <p>Diis persönlicha Gsöff für ${document.getElementById('nameInput').value.trim()}: ${drinkName}</p>
-      <p>Zubereitung: ${instructions}</p>
-      <ul>${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}</ul>
-    `;
+    // drink
+
+    let persDrinkImg = document.getElementById('persDrinkImg');
+    if (persDrinkImg) {
+      persDrinkImg.remove();
+    }
+    persDrinkImg = document.createElement('img');
+    persDrinkImg.id = 'persDrinkImg';
+    persDrinkImg.src = drink.strDrinkThumb;
+    persDrinkImg.alt = drinkName;
+
+    // display drink on page
+    document.getElementById('persDrinkName').insertAdjacentElement('afterend', persDrinkImg);
+    document.getElementById('persDrinkName').textContent = `Diis persönlicha Gsöff für ${document.getElementById('nameInput').value.trim()}: ${drinkName}`;
+    document.getElementById('persDrinkInstructions').textContent = `Zubereitung: ${instructions}`;
+    document.getElementById('persDrinkIngredients').innerHTML = ingredients.map(ingredient => `<li>${ingredient}</li>`).join('');
+
+
   }
 
 function converter(measure) {
@@ -119,26 +135,111 @@ function converter(measure) {
     let ingredients = ingredientsInput.split(',').map(ingredient => ingredient.trim());
 
     // API-Anfrage für Cocktails mit den angegebenen Zutaten
-    let apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredients.join(',')}`;
-    let data = await fetchData(apiUrl);
-    if (data === null) {
+    let allDrinks = [];
+    for (let ingredient of ingredients) {
+      let apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`;
+      let data = await fetchData(apiUrl);
+      if (data !== null) {
+        allDrinks = allDrinks.concat(data.drinks);
+      }
+    }
+
+    let finalDrinks = allDrinks.filter(drink => {
+      let count = allDrinks.filter(d => d.strDrink === drink.strDrink).length;
+      return count === ingredients.length;
+    });
+
+    // remove duplicates
+    let uniqueFinalDrinks = finalDrinks.filter((drink, index) => {
+      return index === finalDrinks.findIndex(d => d.strDrink === drink.strDrink);
+    });
+    
+    if (uniqueFinalDrinks.length === 0) {
       // Error handling
-      document.getElementById('cocktailList').innerText = 'Mit dera Zuatat gits kai Gsöff. (du alki)';
+      document.getElementById('cocktailTable').innerText = 'Mit dera Zuatat gits kai Gsöff. (du alki)';
     } else {
-      displayCocktails(data.drinks);
+      displayCocktails(uniqueFinalDrinks);
     }
   }
 
   // Funktion zum Anzeigen der gefundenen Cocktails
-  function displayCocktails(cocktails) {
-    let cocktailList = document.getElementById('cocktailList');
-    cocktailList.innerHTML = ''; // Clear previous results
-    cocktails.forEach(cocktail => {
-      let cocktailName = cocktail.strDrink;
-      let listItem = document.createElement('li');
-      listItem.textContent = cocktailName;
-      cocktailList.appendChild(listItem);
-    });
+  function displayCocktails(drinks) {
+    let cocktailTable = document.getElementById('cocktailTable');
+    cocktailTable.innerHTML = ''; // Clear previous results
+
+    let table = document.createElement('table');
+    let tbody = document.createElement('tbody');
+
+    drinks.forEach(function(drink) {
+      let row = document.createElement('tr');
+
+      // Bild
+      let drinkCell = document.createElement('td');
+      let image = document.createElement('img');
+      image.src = drink.strDrinkThumb;
+      image.alt = drink.strDrink + " Image";
+      drinkCell.appendChild(image);
+
+      // Name (Link)
+      let nameLink = document.createElement('a');
+      // nameLink.href = drink.idDrink;
+      nameLink.href = '#';
+      nameLink.textContent = drink.strDrink;
+
+      let detailsVisible = false;
+      let detailsElement = null;
+
+      nameLink.addEventListener('click', async function(e) {
+        e.preventDefault();
+        if (!detailsVisible) {
+
+          let apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drink.idDrink}`;
+          let data = await fetchData(apiUrl);
+          let details = data.drinks[0];
+  
+          detailsElement = document.createElement('div');
+
+          let instructions = details.strInstructionsDE;
+          let ingredients = [];
+          for (let i = 1; i <= 15; i++) {
+            let ingredient = details['strIngredient' + i];
+            let measure = details['strMeasure' + i];
+            measure = converter(measure);
+            if (ingredient && measure) {
+              ingredients.push(`${measure.trim()} ${ingredient.trim()}`);
+            }
+          }
+      
+      
+          // display drink on page
+          let instruction = document.createElement('p');
+          instruction.innerHTML = `<p>Zubereitung: ${instructions}</p>`;
+          let ingredientsList = document.createElement('ul');
+          ingredientsList.innerHTML = ingredients.map(ingredient => `<li>${ingredient}</li>`).join('');
+
+          detailsElement.appendChild(instruction);
+          detailsElement.appendChild(ingredientsList);
+
+          nameLink.parentElement.appendChild(detailsElement);
+
+          detailsVisible = true;
+        } else {
+          detailsElement.remove();
+          detailsVisible = false;
+        }
+        
+
+      });
+
+      drinkCell.appendChild(nameLink);
+
+      row.appendChild(drinkCell);
+
+      tbody.appendChild(row);
+  });
+
+  table.appendChild(tbody);
+  cocktailTable.appendChild(table);
   }
 
 // get data from api
